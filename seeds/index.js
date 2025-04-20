97fe08d885cefd5a90d971d228598d5c41c33226
RunyararoMucheche <your_email@example.com>
2025-04-20 10:19:39 -0400

    Create Yelpcamp app and complete user routes

---
+++ seeds/index.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const cities = require("./cities");
+const { places, descriptors } = require("./seedHelpers");
+const Campground = require("../models/campgrounds");
+
+mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", () => {
+  console.log("Database is Connected!");
+});
+
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+  await Campground.deleteMany({});
+  for (let i = 0; i < 50; i++) {
+    const randGen = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 30) + 10;
+    const newCampground = new Campground({
+      title: `${sample(descriptors)} ${sample(places)}`,
+      location: `${cities[randGen].city}, ${cities[randGen].state}`,
+      image: `https://picsum.photos/800?random=${Math.random()}`,
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo quis unde consequuntur debitis rem voluptate asperiores dolor corrupti exercitationem. Veritatis consectetur dolor sequi doloremque, sapiente modi. Voluptatibus amet harum consequatur.",
+      price,
+    });
+    await newCampground.save();
+  }
+};
+
+seedDB().then(() => {
+  mongoose.connection.close();
+});
