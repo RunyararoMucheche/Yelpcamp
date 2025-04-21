884ead60f2b8a00cb9361b63e21cf94d1c634a5c
RunyararoMucheche <your_email@example.com>
2025-04-21 09:30:06 -0400

    Add logout route and middleware to limit user access when not logged in

--- routes/users.js
+++ routes/users.js
@@ -40,4 +40,14 @@ router.post(
   }
 );
 
+router.get("/logout", (req, res, next) => {
+  req.logOut((err) => {
+    if (err) {
+      return next(err);
+    }
+    req.flash("success", "Goodbye. Thank you");
+    res.redirect("/campgrounds");
+  });
+});
+
 module.exports = router;
