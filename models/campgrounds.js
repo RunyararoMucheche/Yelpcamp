a46540789a166ca2e76a4a21b435c321bcbab230
RunyararoMucheche <your_email@example.com>
2025-05-01 19:01:16 -0400

    Reduce image size on edit page

--- models/campgrounds.js
+++ models/campgrounds.js
@@ -3,14 +3,17 @@ const Review = require("./review");
 const User = require("./user");
 const Schema = mongoose.Schema;
 
+const ImageSchema = new Schema({
+  url: String,
+  filename: String,
+});
+
+ImageSchema.virtual("thumbnail").get(function () {
+  return this.url.replace("/upload", "/upload/w_200");
+});
 const campgroundSchema = new Schema({
   title: String,
-  images: [
-    {
-      url: String,
-      filename: String,
-    },
-  ],
+  images: [ImageSchema],
   price: Number,
   location: String,
   description: String,
